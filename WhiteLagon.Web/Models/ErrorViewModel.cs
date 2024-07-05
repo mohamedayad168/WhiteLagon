2e2e226f56f810960c6d507edebe1e08a1dcd15b
Mohamed Ayad <mo7amed3yad@gmail.com>
2024-07-06 01:28:56 +0300

    Add project files.

---
+++ WhiteLagon.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WhiteLagon.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
